62fcd2540f4a3b712b603b38b629bf066c1fcb0a
Juan Sebastian Grajales <grajalesjuansebastian2002@gmail.com>
2024-03-15 20:38:35 -0500

    Añadí un print para comprobar en GitHub

--- hellogit.py
+++ hellogit.py
@@ -3,3 +3,5 @@ print("Hello GIT")
 print("Añadí esta línea para probar el git")
 
 print("Código que quiero añadir a la rama_1")
+
+print("Este print es para probar que los cambios se suben en el GitHub")
